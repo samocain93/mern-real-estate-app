9a2b5a0bb7a264f6d9e37f4004a051c7ba3d9c97
Sam O'Cain <samocain93@gmail.com>
2023-08-28 20:47:56 -0500

    cancel booking endpoint added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -55,11 +55,42 @@ export const getAllBookings = asyncHandler(async (req, res) => {
 
   try {
     const bookings = await prisma.user.findUnique({
-      where: {email},
-      select: {bookedVisits: true}
-    })
-    res.status(200).send(bookings)
+      where: { email },
+      select: { bookedVisits: true },
+    });
+    res.status(200).send(bookings);
   } catch (error) {
-    throw new Error(error.message)
+    throw new Error(error.message);
+  }
+});
+
+//function to cancel a booking
+export const cancelBooking = asyncHandler(async (req, res) => {
+  const { email } = req.body;
+  const { id } = req.params;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+      select: { bookedVisits: true },
+    });
+
+    const index = user.bookedVisits.findIndex((visit) => visit.id === id);
+
+    if (index === -1) {
+      res.status(404).json({ message: 'Booking not found' });
+    } else {
+      user.bookedVisits.splice(index, 1);
+      await prisma.user.update({
+        where: { email },
+        data: {
+          bookedVisits: user.bookedVisits,
+        },
+      });
+
+      res.send("Booking cancelled successfully")
+    }
+  } catch (error) {
+    throw new Error(error.message);
   }
 });
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
   bookVisit,
+  cancelBooking,
   createUser,
   getAllBookings,
 } from '../controllers/userController.js';
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post('/register', createUser);
 router.post('/book/:id', bookVisit);
 router.post('/bookings', getAllBookings);
+router.post('/cancelBooking/:id', cancelBooking);
 
 export { router as userRoute };
